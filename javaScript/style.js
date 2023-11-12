496b9a121c1b3b5719b690a4e8581be47afc57f6
mdhamimhowladerasif <mdhamimhowladerasif@gmail.com>
2023-11-13 02:06:42 +0600

    first commit

---
+++ javaScript/style.js
@@ -0,0 +1,12 @@
+// const sections = document.querySelectorAll('section');
+// for(const section of sections){
+//     section.style.border = '2px solid red';
+//     section.style.marginBottom = '10px';
+//     section.style.borderRadius = '15px';
+//     section.style.paddingLeft = '7px';
+// }
+
+const placeList = document.getElementById('hal');
+const li = document.createElement('li');
+li.innerText = 'Hi I am'
+placeList.appendChild(li);
